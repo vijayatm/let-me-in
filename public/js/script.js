d83735c07b6ac26cf67f004d630469d9bc256692
vijayatm <iamvijayatm@gmail.com>
2021-12-08 22:57:17 +0530

    card structure issue fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,42 @@
 const browserElement = document.getElementById("browser");
-const ipaddressElement = document.getElementById("ipaddress");
-let apiKey = '1be9a6884abd4c3ea143b59ca317c6b2';
-$.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
-  console.log(JSON.stringify(data, null, 2));
-});
-
+const baseApi = 'https://api.github.com/'
+console.log(browserElement);
 
 getUser();
 
 
 async function getUser(){
-    createBrowserCard();  
-    createIpAddressCard();
+  
+    
+    createUserCard();  
+
+
+    
+   
 }
 
 
 
-function createBrowserCard(){
+function createUserCard(){
+   
 var browserName = $.browser.name;
 var browserVersion = $.browser.version;
 var isDesktopUser = $.browser.desktop;
 
-    const browserCard = `
+switch(browserName){
+    case "chrome": 
+        var url = "https://upload.wikimedia.org/wikipedia/commons/a/a5/Google_Chrome_icon_%28September_2014%29.svg";
+}
+
+
+       const browserCard = `
        <table class="table">
-        <thead>
-            <tr class='theadCss'>
-                <th scope="col">Property</th>
-                <th scope="col">Value</th>
-            </tr>
-        </thead>
+       <thead>
+           <tr class='theadCss'>
+               <th scope="col">Property</th>
+               <th scope="col">Value</th>
+           </tr>
+       </thead>
        <tbody>
 
            <tr>
@@ -45,7 +53,7 @@ var isDesktopUser = $.browser.desktop;
                 <td>${isDesktopUser}</td>
             </tr>
        </tbody>
-         </table>
+   </table>
        `;
 
 
@@ -53,41 +61,7 @@ var isDesktopUser = $.browser.desktop;
 }
 
 
-function createIpAddressCard(){
-    var browserName = $.browser.name;
-    var browserVersion = $.browser.version;
-    var isDesktopUser = $.browser.desktop;
-    
-        const ipaddressCard = `
-           <table class="table">
-            <thead>
-                <tr class='theadCss'>
-                    <th scope="col">Property</th>
-                    <th scope="col">Value</th>
-                </tr>
-            </thead>
-           <tbody>
-    
-               <tr>
-                   <th scope="row">Browser Name</th>
-                   <td>${browserName}</td>
-               </tr>
-    
-               <tr>
-                    <th scope="row">Browser Version</th>
-                    <td>${browserVersion}</td>
-                </tr>
-                <tr>
-                    <th scope="row">Using from Desktop</th>
-                    <td>${isDesktopUser}</td>
-                </tr>
-           </tbody>
-             </table>
-           `;
-    
-    
-           ipaddressElement.innerHTML = ipaddressCard;
-    }
+
 
 
 
